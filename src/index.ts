e3748de0e5f0e4cbcbf91e7b6025652778993b14
ZernoBilly <heikki.kurppa@gmail.com>
2021-12-15 22:02:01 +0200

    Create user and has password

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,29 @@
 import express from "express";
 import authRoutes from "./routes/auth";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 const app = express();
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+const DB_CONNECTION = process.env.DB_CONNECTION as string;
+
+mongoose
+  .connect(DB_CONNECTION)
+  .then(() => {
+    console.log("Connected to database");
+
+    app.listen(PORT, () => {
+      console.log(`Server listening to port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log({ error });
+    throw new Error(error);
+  });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/auth", authRoutes);
-
-app.listen(5002, () => {
-  console.log("Running on port 5002");
-});
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import { body, validationResult } from "express-validator";
+import User from "../models/user";
+import bcrypt from "bcryptjs";
 
 const router = express.Router();
 
@@ -9,7 +11,7 @@ router.post(
   body("password").isLength({ min: 5 }).withMessage("Password is too short."),
   async (req, res) => {
     const validationErrors = validationResult(req);
-    const { email, password } = await req.body;
+    const { name, email, password } = await req.body;
 
     if (!validationErrors.isEmpty()) {
       const errors = validationErrors.array().map((error) => {
@@ -17,10 +19,31 @@ router.post(
           msg: error.msg,
         };
       });
-      return res.json({ errors });
+      return res.json({ errors, data: null });
     }
 
-    res.json({ email, password });
+    const user = await User.findOne({ email });
+
+    if (user) {
+      res.json({
+        errors: [
+          {
+            msg: "Email already in use",
+          },
+        ],
+        data: null,
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    res.json({ user });
   }
 );
 
